57596100763a506fef747179d12649cd80412bbc
Oscar Torres <Oscar Torres@10.20.13.148>
2016-05-31 11:25:09 -0500

    Se agrego parte del metodo

--- SyFP16/src/E1/Cesar.java
+++ SyFP16/src/E1/Cesar.java
@@ -14,11 +14,31 @@ public class Cesar {
      
      public String cifrar(String m, int c)
      {
+         String resultado = "";
          for(int i = 0; i < m.length(); i++)
          {
-            char 
+           resultado = resultado + (char)(m.charAt(i) + c); 
          }
-         return m; 
+         return resultado; 
+        
      }
      
+     public String descifrar(String m, int c)
+     {
+         String resultado = "";
+         for(int i = 0; i < m.length(); i++)
+         {
+            resultado = resultado + (char)(m.charAt(i) + c);   
+         }
+         
+         return resultado;
+     }
+     public static void main(String[] args) {
+        Cesar cc = new Cesar();
+         
+        String s = cc.cifrar("hola", 2);
+        String d = cc.descifrar("jqnc",-2);
+         System.out.println(s);
+         System.out.println(d);
+   }
 }
